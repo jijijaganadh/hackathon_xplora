05e4e3800c6805edebdc23d584bc1a24a47c01cc
ADeeBz23 <designeradeeba@gmail.com>
2023-01-28 11:32:56 +0530

    Merge branch 'master' of https://github.com/jijijaganadh/hackathon_xplora

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 
 app_name = 'main'
@@ -36,6 +38,29 @@ urlpatterns = [
     path("deletementor/", views.DeleteMentor.as_view(), name="deletementor"),
     path("delete-member/<int:id>", views.DeleteMember.as_view(), name="deletemember"),
     path("viewproblem/", views.ViewProblemdetails.as_view(), name='viewproblem'),
-
+    path('activate-user/<uidb64>/<token>',views.activate_user, name='activate'),
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(
+             template_name='commons/password-reset/password_reset_form.html',
+             subject_template_name='commons/password-reset/password_reset_subject.txt',
+             email_template_name='commons/password-reset/password_reset_email.html',
+             # success_url='/login/'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='commons/password-reset/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='commons/password-reset/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='commons/password-reset/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
 ]
 
